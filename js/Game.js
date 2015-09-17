f02ba67f7a1fb13c876cdc12466daf1c51ad8f4a
Ian <ianramosc@gmail.com>
2015-09-16 21:29:18 -0300

    added an easier syntax for a better comprehension

--- js/Game.js
+++ js/Game.js
@@ -1,17 +1,14 @@
-var Unidev = {}
-	,	background;
+var game = new Phaser.Game(800, 600, Phaser.AUTO);
 
-Unidev.Game = function(game) {};
+function preload() {
+	this.load.image('background', 'https://raw.githubusercontent.com/photonstorm/phaser-examples/master/examples/assets/games/invaders/starfield.png');
+}
 
-Unidev.Game.prototype = {
-	preload: function() {
-		this.load.image('background', 'https://raw.githubusercontent.com/photonstorm/phaser-examples/master/examples/assets/games/invaders/starfield.png');
-	},
-	create: function() {
-		this.physics.startSystem(Phaser.Physics.ARCADE);
-		background = this.add.tileSprite(0, 0, 800, 600, 'background');
-	},
-	update: function() {
-		background.tilePosition.y += 2;
-	}
-};
+function create() {
+	this.physics.startSystem(Phaser.Physics.ARCADE);
+	background = this.add.tileSprite(0, 0, 800, 600, 'background');
+}
+
+function update() {
+	background.tilePosition.y += 2;
+}
--- js/Unidev.js
+++
@@ -1,5 +0,0 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO);
-
-game.state.add('Game', Unidev.Game);
-
-game.state.start('Game');
