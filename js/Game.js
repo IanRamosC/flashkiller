43d69fa212872810a52cf899f2e66b3f1515d1f0
Ian <ianramosc@gmail.com>
2015-09-18 21:44:57 -0300

    bullets and enemies added, its not working as well

--- js/Game.js
+++ js/Game.js
@@ -1,9 +1,11 @@
 var background
 	,	player
 	,	playerSpeed = 200
-	,	enemies = {}
-	, fireBalls
-	, fireBall
+	,	enemies
+	,	enemy
+	,	enemySpeed = 150
+	, bullets
+	, bullet
 	,	shotSpeed = 300
 	, shotTime = 0
 	, keys;
@@ -13,23 +15,28 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create:
 function preload() {
 	game.load.image('background', 'https://raw.githubusercontent.com/photonstorm/phaser-examples/master/examples/assets/games/invaders/starfield.png');
 	game.load.image('spaceship', 'assets/img/spaceship.png');
-	game.load.image('fireball', 'https://support.ouat-e.com/media/luckypirate/images/help/tool_fireball.png');
+	game.load.image('enemies', 'assets/img/enemies.png');
+	game.load.image('bullet', 'assets/img/bullet.png');
 }
 
 function create() {
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 	background = game.add.tileSprite(0, 0, 800, 600, 'background');
 
-	fireBalls = game.add.group();
-	fireBalls.enableBody = true;
-	fireBalls.physicsBodyType = Phaser.Physics.ARCADE;
-	fireBalls.createMultiple(30, 'fireball');
-	fireBalls.setAll('anchor.x', 0.5);
-	fireBalls.setAll('anchor.y', 0.5);
-  fireBalls.setAll('outOfBoundsKill', true);
-  fireBalls.setAll('checkWorldBounds', true);
+	bullets = game.add.group();
+	bullets.enableBody = true;
+	bullets.physicsBodyType = Phaser.Physics.ARCADE;
+	bullets.createMultiple(30, 'bullet');
+	bullets.setAll('anchor.x', 0.5);
+	bullets.setAll('anchor.y', 0.5);
+  bullets.setAll('outOfBoundsKill', true);
+  bullets.setAll('checkWorldBounds', true);
 
 	createPlayer();
+
+	enemies = game.add.physicsGroup(Phaser.Physics.ARCADE, game.world, 'enemies');
+
+	createEnemies();
 
 	keys = game.input.keyboard.createCursorKeys();
 	fireUp = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
@@ -37,6 +44,8 @@ function create() {
 
 function update() {
 	background.tilePosition.x -= 2;
+
+	game.physics.arcade.overlap(bullet, enemies, killEnemy, null, this);
 
   player.body.velocity.setTo(0, 0);
 
@@ -55,6 +64,7 @@ function update() {
 	if(fireUp.isDown) {
 		shot();
 	}
+
 }
 
 function createPlayer() {
@@ -66,13 +76,24 @@ function createPlayer() {
 
 function shot() {
 	if(game.time.now > shotTime) {
-		fireBall = fireBalls.getFirstExists(false);
-		if(fireBall) {
-			fireBall.reset(player.x + player.width, player.y);
-			fireBall.body.velocity.x = shotSpeed;
-			fireBall.scale.x = 0.5;
-			fireBall.scale.y = 0.5;
+		bullet = bullets.getFirstExists(false);
+		if(bullet) {
+			bullet.reset(player.x + player.width, player.y);
+			bullet.body.velocity.x = shotSpeed;
 			shotTime = game.time.now + 300;
 		}
 	}
+}
+
+function createEnemies() {
+	var x = game.world.width
+		,	y = game.rnd.integerInRange(0, game.world.height);
+	enemy = enemies.create(x, y, 'enemies');
+	enemy.health = 3;
+	enemy.body.velocity.x = -enemySpeed;
+}
+
+function killEnemy(bullet, enemy) {
+	enemy.damage(1);
+	bullet.kill();
 }
