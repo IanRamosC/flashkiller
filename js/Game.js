782325c3a0bfa5600722b40c06e35e58e8783d8c
Ian <ianramosc@gmail.com>
2015-10-07 19:02:35 -0300

    up

--- js/Game.js
+++ js/Game.js
@@ -20,11 +20,11 @@ function preload() {
 }
 
 function create() {
-  game.scale.pageAlignHorizontally = true;
-  game.scale.pageAlignVertically = true;
+	game.scale.pageAlignVertically = true;
+	game.scale.pageAlignHorizontally = true;
 
-	game.physics.startSystem(Phaser.Physics.ARCADE);
 	background = game.add.tileSprite(0, 0, 800, 600, 'background');
+	game.physics.startSystem(Phaser.Physics.ARCADE);
 
 	bullets = game.add.group();
 	bullets.enableBody = true;
@@ -37,16 +37,19 @@ function create() {
 
 	enemies = game.add.physicsGroup(Phaser.Physics.ARCADE, game.world, 'enemies');
 
- 	game.enemyGenerator = game.time.events.loop(Phaser.Timer.SECOND * 1.25, createEnemies, this);
+	game.enemyGenerator = game.time.events.loop(Phaser.Timer.SECOND * 1.25, createEnemies, this);
   game.enemyGenerator.timer.start();
 
   infoText.currentScore = 0;
   infoText.scoreText = game.add.text(10, 10, 'Score: ' + infoText.currentScore, { font: '34px Arial', fill: '#FFF'} );
+  infoText.highscore = 0;
+  infoText.highscoreText = game.add.text(game.world.width - 300, 10, 'Highscore: ' + infoText.highscore, { font: '34px Arial', fill: '#FFF'} );
 
 	Keys.cursors = game.input.keyboard.createCursorKeys();
 	Keys.fireUp = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 	Keys.pause = game.input.keyboard.addKey(Phaser.Keyboard.ESC);
 	Keys.pause.onDown.add(pauseGame, this);
+
 }
 
 function update() {
@@ -66,6 +69,7 @@ function update() {
 	if(Keys.cursors.left.isDown && player.body.x > 10) {
 		player.body.velocity.x = -playerSpeed;
 	}
+
 	if(Keys.fireUp.isDown) {
 		shot();
 	}
@@ -73,15 +77,17 @@ function update() {
 	game.physics.arcade.overlap(bullets, enemies, killEnemy, null, this);
 	game.physics.arcade.overlap(player, enemies, hitPlayer, null, this);
 	infoText.scoreText.text = 'Score: ' + infoText.currentScore;
+	infoText.highscoreText.text = 'Highscore: ' + infoText.highscore;
 
 }
 
-function createPlayer() {
+ function createPlayer() {
 	player = game.add.sprite(10, game.world.height/2, 'spaceship');
   game.physics.arcade.enable(player);
 	player.anchor.setTo(0, 0.5);
   player.body.collideWorldBounds = true;
-}
+ }
+
 
 function shot() {
 	if(game.time.now > shotTime) {
@@ -118,14 +124,22 @@ function hitPlayer(player, enemy) {
 	restart();
 }
 
-function pauseGame() {
-	game.paused = !game.paused;
-}
-
 function restart() {
+	if (!!localStorage) {
+    infoText.highscore = localStorage.getItem('flashkiller_highscore');
+
+    if(!infoText.highscore || infoText.highscore < infoText.currentScore) {
+      infoText.highscore = infoText.currentScore;
+      localStorage.setItem('flashkiller_highscore', infoText.highscore);
+    }
+  }
   enemies.removeAll();
   createEnemies();
   createPlayer();
 
 	infoText.currentScore = 0;
+}
+
+function pauseGame() {
+	game.paused = !game.paused;
 }
